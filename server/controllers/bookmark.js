6c0d374a7bbc905039b99ce7e2a02ff13221c804
Khoi <bachthienkhoi2001@gmail.com>
2021-12-03 22:10:49 +0700

    Create GET bookmark API

--- server/controllers/bookmark.js
+++ server/controllers/bookmark.js
@@ -1,6 +1,42 @@
 import { Bookmark } from "../models/Bookmark.js";
 
-export const getBookmark = async (req, res) => {};
+export const getBookmark = async (req, res) => {
+    const { userId } = req.body;
+
+    try {
+        const bookmarks = await Bookmark.aggregate([
+            {
+                $match: { userId: parseInt(userId) },
+            },
+            {
+                $lookup: {
+                    from: "novels",
+                    localField: "novelId",
+                    foreignField: "novelId",
+                    as: "novel",
+                },
+            },
+            {
+                $unwind: {
+                    path: "$novel",
+                    preserveNullAndEmptyArrays: true,
+                },
+            },
+            {
+                $project: {
+                    _id: 0,
+                    novelId: "$novelId",
+                    title: "$novel.title",
+                    cover: "$novel.cover",
+                },
+            },
+        ]);
+
+        res.status(200).json({ bookmarks });
+    } catch (error) {
+        res.status(500).json({ error });
+    }
+};
 
 export const createBookmark = async (req, res) => {
     const { userId, novelId } = req.body;
--- server/routes/bookmark.js
+++ server/routes/bookmark.js
@@ -1,10 +1,15 @@
 import express from "express";
 import { verifyToken } from "../middlewares/auth.js";
 import { verifyNovelId } from "../middlewares/novel.js";
-import { createBookmark, deleteBookmark } from "../controllers/bookmark.js";
+import {
+    getBookmark,
+    createBookmark,
+    deleteBookmark,
+} from "../controllers/bookmark.js";
 
 const router = express.Router({ mergeParams: true });
 
+router.get("/", verifyToken, getBookmark);
 router.post("/", verifyToken, verifyNovelId, createBookmark);
 router.delete("/", verifyToken, verifyNovelId, deleteBookmark);
 
