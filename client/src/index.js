0ba5f5b0a94c0d2e4af6b5369eb6f380619448d5
Khoi <bachthienkhoi2001@gmail.com>
2022-01-16 00:16:03 +0700

    Update port

--- client/src/index.js
+++ client/src/index.js
@@ -1,45 +1,43 @@
-const express = require('express');
-const exphbs = require('express-handlebars');
-const path = require('path');
+const express = require("express");
+const exphbs = require("express-handlebars");
+const path = require("path");
 const app = express();
-const methodOverride = require('method-override');
-const cors = require('cors')
+const methodOverride = require("method-override");
+const cors = require("cors");
 
- 
 app.use(cors());
 
 // port
-const port = 3000 || process.env.PORT ;
+const port = process.env.PORT || 3000;
 
 // public file
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // express-handlebars
 const hbs = exphbs.create({
-    extname: 'hbs',
+    extname: "hbs",
 });
-app.engine('hbs', hbs.engine);
-app.set('view engine', 'hbs');
-app.set('views', './src/views');
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
+app.set("views", "./src/views");
 
 // Middleware: read body of post method
 app.use(
     express.urlencoded({
         extended: true,
-    }),
+    })
 );
 app.use(express.json());
 
 // override with the X-HTTP-Method-Override header in the request
-app.use(methodOverride('_method'));
+app.use(methodOverride("_method"));
 
-app.get('/', (req, res) => {
-    res.redirect('/home');
+app.get("/", (req, res) => {
+    res.redirect("/home");
 });
 // router
-const route = require('./routes');
+const route = require("./routes");
 route(app);
-
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
