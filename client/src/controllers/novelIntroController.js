5af1d496bea963c7c96ea141060a365fc4dfae9a
Khoi <bachthienkhoi2001@gmail.com>
2022-01-16 08:59:12 +0700

    update

--- client/src/controllers/novelIntroController.js
+++ client/src/controllers/novelIntroController.js
@@ -1,34 +1,29 @@
-
 class novelIntroController {
-
     home(req, res, next) {
-        const novelID =  req.params.id;
-        res.render('novelIntro/home', {
-            layout: 'novelIntro',
-            css: ['novelIntro', 'novelIntroResponsive'],
-            js: ['header', 'isSignedIn'],
-            jsIn: ['Fetch', 'Rating', 'ReadMore', 'ShowEmoticon', 'comment'],
+        const novelID = req.params.id;
+        res.render("novelIntro/home", {
+            layout: "novelIntro",
+            css: ["novelIntro", "novelIntroResponsive"],
+            js: ["header", "isSignedIn"],
+            jsIn: ["Fetch", "Rating", "ReadMore", "ShowEmoticon", "comment"],
             novelID: novelID,
-            
-        })
+        });
         return;
     }
 
     chapter(req, res, next) {
-        const novelID =  req.params.id;
-        const chapterID =  req.params.chapterId;
-      
-        res.render('novelChapter/home', {
-            layout: 'novelChapter',
-            css: ['chapterPage', 'baseChapterPage', 'responsiveChapterPage'],
-            js: ['isSignedIn', 'header', 'chapterPage'],
+        const novelID = req.params.id;
+        const chapterID = req.params.chapterId;
+
+        res.render("novelChapter/home", {
+            layout: "novelChapter",
+            css: ["chapterPage", "baseChapterPage", "responsiveChapterPage"],
+            js: ["isSignedIn", "header", "chapterPage"],
             novelID: novelID,
             chapterID: chapterID,
-            
-        })
+        });
         return;
     }
-
 }
 
 module.exports = new novelIntroController();
